dbd48faa1081d6397ff23156e487f807cbe73da6
DepersonalizedCookie <batorfidada@gmail.com>
2020-08-16 20:02:30 +0200

    order.html has been updated with added JS operations(removable alert, new select element) & CSS modifications.

--- js/main.js
+++ js/main.js
@@ -23,5 +23,59 @@ function showSumPrice(price = 2000, amountNumber= 1) {
     
 }
 
+//Window events
 
+let sendButton = document.querySelector("form .btn.btn-success");
+/*sendButton.onclick = function() {
+    alert("Thank you! You order is on the way!")
+}*/
+sendButton.addEventListener("click", function(){
+    alert("Thank you! Your order is on the way!");
+});
 
+window.addEventListener("resize", function() {
+    console.log(this.innerHeight, this.innerWidth);
+});
+
+//Űrlap események
+let orderForm = document.querySelector("#orderForm");
+orderForm.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    
+    let inputs = this.querySelectorAll("input");
+    let values = {};
+    for (let i = 0; i < inputs.length; i++) {
+        values[inputs[i].name] = inputs[i].value;
+    }
+    console.log( values );
+});
+
+//Parent element kezelése
+let alertCloseButtons = document.querySelectorAll(".close[data-dismiss='alert']");
+let alertCloseEventHandlerFunction = function(ev) {
+    this.parentElement.style.display = "none";
+}
+for (let i = 0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction);
+}
+
+//Select elem kitöltése
+let toppings = [
+    "Ketchup",
+    "Hot chili sauce",
+    "Vegan cheese",
+    "Vegan bacon",
+    "Pickles",
+    "Mustard",
+    "Vegan Mayo"
+];
+
+let toppingSelect = document.querySelector("#toppingSelect");
+let index = 0;
+while(index < toppings.length) {
+    let option = document.createElement("option");
+    option.value = index;
+    option.innerHTML = toppings[index];
+    toppingSelect.appendChild(option);
+    index++;
+}
